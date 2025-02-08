ff93e047cf26a2dfdd98edf65a4453db584bd95c
VanwTus09 <tunguyen/05112004@gmail.com>
2025-02-08 16:20:13 +0700

    push

---
+++ Minimal Apis/Minimal Apis/TodoDb.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+public class TodoDb : DbContext
+{
+    public TodoDb(DbContextOptions<TodoDb> options)
+        : base(options) { }
+
+    public DbSet<Todo> Todos => Set<Todo>();
+}
